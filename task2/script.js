5ea1ea70364ae31ea1b395cbe6d68323ed00b61c
akshat-gandhi-zeuslearning <akshat.gandhi@zeuslearning.com>
2024-06-04 17:57:39 +0530

    First-Commit

---
+++ task2/script.js
@@ -0,0 +1,24 @@
+function formSubmit() {
+    var name = document.getElementById("your-name");
+    var comment = document.getElementById("comment-id");
+    var gender_male = document.getElementById("gender_male");
+    var gender_female = document.getElementById("gender_female");
+    /*if (name.value.length === 0 && comment.value.length === 0 && (!gender_male.checked && !gender_female.checked)) {
+        alert("Please enter all details.");
+    }*/
+    
+    if (name.value.length === 0) {
+        alert("Please enter your name.");
+        document.getElementById("your-name").focus();
+    }
+    if(comment.value.length ===0)
+        {
+        alert("Please enter your Comment.");
+        document.getElementById("comment-id").focus();
+        }
+
+    if(!gender_male.checked && !gender_female.checked )
+        {
+            alert("Please enter gender")
+        }   
+}
